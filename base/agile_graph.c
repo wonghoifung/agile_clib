327471f9eca215aec05ded9d9221eb43a8a6aa8b
wonghoifung <wonghoifung85@gmail.com>
2016-02-20 08:29:13 +0800

    init destroy

--- base/agile_graph.c
+++ base/agile_graph.c
@@ -2,11 +2,24 @@
 #include "agile_graph.h"
 
 void agile_graph_init(agile_graph* graph, int(*match)(const void*,const void*), void(*destroy)(void*)) {
-
+	graph->vcount = 0;
+	graph->ecount = 0;
+	graph->match = match;
+	graph->destroy = destroy;
+	agile_list_init(&graph->adjlists, NULL);
 }
 
 void agile_graph_destroy(agile_graph* graph) {
-
+	agile_adj_list* adjlist;
+	while (agile_list_size(&graph->adjlists) > 0) {
+		if (agile_list_rem_next(&graph->adjlists, NULL, (void**)&adjlist) == 0) {
+			agile_set_destroy(&adjlist->adjacent);
+			if (graph->destroy != NULL) graph->destroy(adjlist->vertex);
+			free(adjlist);
+		}
+	}
+	agile_list_destroy(&graph->adjlists);
+	memset(graph, 0, sizeof(agile_graph));
 }
 
 int agile_graph_ins_vertex(agile_graph* graph, const void* data) {
