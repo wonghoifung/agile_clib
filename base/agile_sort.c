5e71c94c7bd06feb3684cb3b79483435e6cbd844
wonghoifung <wonghoifung85@gmail.com>
2016-02-21 16:49:03 +0800

    merge sort

--- base/agile_sort.c
+++ base/agile_sort.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 #include "agile_sort.h"
 
 int issort(void* data, int size, int esize, int(*compare)(const void*,const void*)) {
@@ -66,6 +67,64 @@ int qksort(void* data, int size, int esize, int i, int k, int(*compare)(const vo
 	return 0;
 }
 
+static void dump_int_data(void* data, int size) {
+	int i=0;
+	for (; i<size; ++i) {
+		printf("%d ",((int*)data)[i]);
+	}
+	printf("\n");
+}
+
+static int merge(void* data, int esize, int i, int j, int k, int(*compare)(const void*,const void*)) {
+	char* a = data;
+	char* m;
+	int ipos,jpos,mpos;
+	ipos = i;
+	jpos = j + 1;
+	mpos = 0;
+	if ((m=(char*)malloc(esize*((k-i)+1)))==NULL) return -1;
+	while (ipos <= j || jpos <= k) {
+		if (ipos > j) {
+			while (jpos <= k) {
+				memcpy(&m[mpos*esize], &a[jpos*esize], esize);
+				jpos+=1;
+				mpos+=1;
+			}
+			continue;
+		} else if (jpos > k) {
+			while (ipos <= j) {
+				memcpy(&m[mpos*esize], &a[ipos*esize], esize);
+				ipos+=1;
+				mpos+=1;
+			}
+			continue;
+		}
+		if (compare(&a[ipos*esize], &a[jpos*esize]) < 0) {
+			memcpy(&m[mpos*esize], &a[ipos*esize], esize);
+			ipos+=1;
+			mpos+=1;
+		} else {
+			memcpy(&m[mpos*esize], &a[jpos*esize], esize);
+			jpos+=1;
+			mpos+=1;
+		}
+	}
+	memcpy(&a[i*esize], m, esize*((k-i)+1));
+	free(m);
+	return 0;
+}
+
+int mgsort(void* data, int size, int esize, int i, int k, int(*compare)(const void*,const void*)) {
+	int j;
+	if (i < k) {
+		j = (int)((i+k-1)/2);
+		if (mgsort(data, size, esize, i, j, compare) < 0) return -1;
+		if (mgsort(data, size, esize, j+1, k, compare) < 0) return -1;
+		if (merge(data, esize, i, j, k, compare) < 0) return -1;
+	}
+	return 0;
+}
+
 //////////////////////////////
 
 //#include "test_common.h"
@@ -75,11 +134,11 @@ void test_agile_sort() {
 	int arr[] = {5,7,9,1,3,8,6,2,0,4};
 	int size = sizeof(arr)/sizeof(arr[0]);
 	int esize = sizeof(arr[0]);
-	//issort(arr,size,esize,compare_int);
-	qksort(arr, size, esize, 0, size-1, compare_int);
-	int i=0;
-	for (; i<size; ++i) {
-		printf("%d ",arr[i]);
-	}
-	printf("\n");
+	int res;
+	dump_int_data(arr, size);
+	//res = issort(arr,size,esize,compare_int);
+	//res = qksort(arr, size, esize, 0, size-1, compare_int);
+	res = mgsort(arr, size, esize, 0, size-1, compare_int);
+	printf("res: %d\n", res);
+	dump_int_data(arr, size);
 }
--- base/agile_sort.h
+++ base/agile_sort.h
@@ -3,6 +3,7 @@
 
 int issort(void* data, int size, int esize, int(*compare)(const void*,const void*));
 int qksort(void* data, int size, int esize, int i, int k, int(*compare)(const void*,const void*));
+int mgsort(void* data, int size, int esize, int i, int k, int(*compare)(const void*,const void*));
 
 void test_agile_sort();
 
