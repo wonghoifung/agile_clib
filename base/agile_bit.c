04b8b53f8037deba5d2a1ee60e0864f74ec8754e
wonghoifung <wonghoifung85@gmail.com>
2016-02-22 07:49:30 +0800

    bit operation kickoff

--- base/agile_bit.c
+++ base/agile_bit.c
@@ -1,4 +1,21 @@
+#include <string.h>
+#include "agile_bit.h"
 
+int agile_bit_get(const unsigned char* bits, int pos) {
+
+}
+
+void agile_bit_set(unsigned char* bits, int pos, int state) {
+
+}
+
+void agile_bit_xor(const unsigned char* bits1, const unsigned char* bits2, unsigned char* bitsx, int size) {
+
+}
+
+void bit_rot_left(unsigned char* bits, int size, int count) {
+
+}
 
 //////////////////////////////
 
--- base/agile_bit.h
+++ base/agile_bit.h
@@ -1,6 +1,10 @@
 #ifndef agile_bit_header
 #define agile_bit_header
 
+int agile_bit_get(const unsigned char* bits, int pos);
+void agile_bit_set(unsigned char* bits, int pos, int state);
+void agile_bit_xor(const unsigned char* bits1, const unsigned char* bits2, unsigned char* bitsx, int size);
+void bit_rot_left(unsigned char* bits, int size, int count);
 
 void test_agile_bit();
 
