8e85e465187184ca2be8ac0fd3b620368df1fcbd
wonghoifung <wonghoifung85@gmail.com>
2016-02-22 16:56:30 +0800

    build huffman tree

--- base/agile_compress.c
+++ base/agile_compress.c
@@ -1,4 +1,110 @@
+#include <limits.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
 #include "agile_compress.h"
+#include "agile_bit.h"
+#include "agile_pqueue.h"
+
+static int compare_freq(const void* tree1, const void* tree2) {
+	agile_huff_node* root1;
+	agile_huff_node* root2;
+	root1 = (agile_huff_node*)agile_bitree_data(agile_bitree_root((const agile_bitree*)tree1));
+	root2 = (agile_huff_node*)agile_bitree_data(agile_bitree_root((const agile_bitree*)tree2));
+	if (root1->freq < root2->freq) return 1;
+	else if (root1->freq > root2->freq) return -1;
+	else return 0;
+}
+
+// destroy tree from pqueue
+static void destroy_tree(void* tree) {
+	agile_bitree_destroy(tree);
+	free(tree);
+}
+
+static int build_tree(int* freqs, agile_bitree** tree) {
+	agile_bitree* init;
+	agile_bitree* merge;
+	agile_bitree* left;
+	agile_bitree* right;
+	agile_pqueue pqueue;
+	agile_huff_node* data;
+	int size, c;
+	*tree = NULL;
+	agile_pqueue_init(&pqueue, compare_freq, destroy_tree);
+	for (c=0; c<=UCHAR_MAX; ++c) {
+		if (freqs[c] != 0) {
+			if ((init=(agile_bitree*)malloc(sizeof(agile_bitree)))==NULL) {
+				agile_pqueue_destroy(&pqueue);
+				return -1;
+			}
+			agile_bitree_init(init, free);
+			if ((data=(agile_huff_node*)malloc(sizeof(agile_huff_node)))==NULL) {
+				agile_pqueue_destroy(&pqueue);
+				return -1;
+			}
+			data->symbol = c;
+			data->freq = freqs[c];
+			if (agile_bitree_ins_left(init, NULL, data) != 0) {
+				free(data);
+				agile_bitree_destroy(init);
+				free(init);
+				agile_pqueue_destroy(&pqueue);
+				return -1;
+			}
+			if (agile_pqueue_insert(&pqueue, init) != 0) {
+				agile_bitree_destroy(init);
+				free(init);
+				agile_pqueue_destroy(&pqueue);
+				return -1;
+			}
+		}
+	}
+	size = agile_pqueue_size(&pqueue);
+	for (c=1; c<=size-1; ++c) {
+		if ((merge=(agile_bitree*)malloc(sizeof(agile_bitree)))==NULL) {
+			agile_pqueue_destroy(&pqueue);
+			return -1;
+		}
+		if (agile_pqueue_extract(&pqueue, (void**)&left) != 0) {
+			agile_pqueue_destroy(&pqueue);
+			free(merge);
+			return -1;
+		}
+		if (agile_pqueue_extract(&pqueue, (void**)&right) != 0) {
+			agile_pqueue_destroy(&pqueue);
+			free(merge);
+			return -1;
+		}
+		if ((data=(agile_huff_node*)malloc(sizeof(agile_huff_node)))==NULL) {
+			agile_pqueue_destroy(&pqueue);
+			free(merge);
+			return -1;
+		}
+		memset(data, 0, sizeof(agile_huff_node));
+		data->freq = ((agile_huff_node*)agile_bitree_data(agile_bitree_root(left)))->freq +
+					 ((agile_huff_node*)agile_bitree_data(agile_bitree_root(right)))->freq;
+		if (agile_bitree_merge(merge, left, right, data) != 0) {
+			agile_pqueue_destroy(&pqueue);
+			free(merge);
+			return -1;
+		}
+		if (agile_pqueue_insert(&pqueue, merge) != 0) {
+			agile_pqueue_destroy(&pqueue);
+			agile_bitree_destroy(merge);
+			free(merge);
+			return -1;
+		}
+		free(left);
+		free(right);
+	}
+	if (agile_pqueue_extract(&pqueue, (void**)tree) != 0) {
+		agile_pqueue_destroy(&pqueue);
+		return -1;
+	}
+	agile_pqueue_destroy(&pqueue);
+	return 0;
+}
 
 int agile_huffman_compress(const unsigned char* original, unsigned char** compressed, int size) {
 
