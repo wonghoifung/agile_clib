1a21e61d8e340617a3f07bca8e12f830185c6a4a
wonghoifung <wonghoifung85@gmail.com>
2016-02-23 21:56:29 +0800

    encrypt

--- base/agile_encrypt.c
+++ base/agile_encrypt.c
@@ -1,5 +1,24 @@
-
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include "agile_bit.h"
 #include "agile_encrypt.h"
+
+void agile_des_encipher(const unsigned char* plaintext, unsigned char* ciphertext, const unsigned char* key) {
+
+}
+
+void agile_des_decipher(const unsigned char* ciphertext, unsigned char* plaintext, const unsigned char* key) {
+
+}
+
+void agile_rsa_encipher(Huge plaintext, Huge* ciphertext, agile_rsa_pub_key pubkey) {
+
+}
+
+void agile_rsa_decipher(Huge ciphertext, Huge* plaintext, agile_rsa_pri_key prikey) {
+
+}
 
 //////////////////////////////
 
--- base/agile_encrypt.h
+++ base/agile_encrypt.h
@@ -1,6 +1,24 @@
 #ifndef agile_encrypt_header
 #define agile_encrypt_header
 
+typedef unsigned long Huge;
+
+typedef struct agile_rsa_pub_key_ {
+	Huge e;
+	Huge n;
+} agile_rsa_pub_key;
+
+typedef struct agile_rsa_pri_key_ {
+	Huge d;
+	Huge n;
+} agile_rsa_pri_key;
+
+void agile_des_encipher(const unsigned char* plaintext, unsigned char* ciphertext, const unsigned char* key);
+void agile_des_decipher(const unsigned char* ciphertext, unsigned char* plaintext, const unsigned char* key);
+
+void agile_rsa_encipher(Huge plaintext, Huge* ciphertext, agile_rsa_pub_key pubkey);
+void agile_rsa_decipher(Huge ciphertext, Huge* plaintext, agile_rsa_pri_key prikey);
+
 void test_agile_encrypt();
 
 #endif
