e4efdb186baf5a398f5d5f6aae99ce14c957ef9b
wonghoifung <wonghoifung85@gmail.com>
2016-03-11 02:26:39 -0500

    test mod

--- cii/test.c
+++ cii/test.c
@@ -6,5 +6,14 @@ int main() {
 	int y = 5;
 	printf("%d\n", Arith_div(x,y));
 	printf("%d\n", x/y);
+	
+	int N = 10;
+	printf("%d\n", Arith_mod(10,N));
+	printf("%d\n", Arith_mod(11,N));
+	printf("%d\n", Arith_mod(0-1,N));
+
+	printf("%d\n", (10%N));
+	printf("%d\n", (11%N));
+	printf("%d\n", ((0-1)%N));
 	return 0;
 }
