9ad3c552551e24f346eb31fe0994ffcb820f3eb3
wonghoifung <wonghoifung85@gmail.com>
2016-03-18 18:32:40 +0800

    stack

--- cii/test.c
+++ cii/test.c
@@ -1,19 +1,30 @@
-#include "arith.h"
+// #include "arith.h"
+#include "stack.h"
 #include <stdio.h>
 
 int main() {
-	int x = -13;
-	int y = 5;
-	printf("%d\n", Arith_div(x,y));
-	printf("%d\n", x/y);
+	// int x = -13;
+	// int y = 5;
+	// printf("%d\n", Arith_div(x,y));
+	// printf("%d\n", x/y);
 	
-	int N = 10;
-	printf("%d\n", Arith_mod(10,N));
-	printf("%d\n", Arith_mod(11,N));
-	printf("%d\n", Arith_mod(0-1,N));
+	// int N = 10;
+	// printf("%d\n", Arith_mod(10,N));
+	// printf("%d\n", Arith_mod(11,N));
+	// printf("%d\n", Arith_mod(0-1,N));
 
-	printf("%d\n", (10%N));
-	printf("%d\n", (11%N));
-	printf("%d\n", ((0-1)%N));
+	// printf("%d\n", (10%N));
+	// printf("%d\n", (11%N));
+	// printf("%d\n", ((0-1)%N));
+
+	Stack_T st = Stack_new();
+	printf("empty:%d\n", Stack_empty(st));
+	Stack_push(st, (void*)123);
+	Stack_push(st, (void*)456);
+	printf("empty:%d\n", Stack_empty(st));
+	printf("%ld\n", (long)Stack_pop(st));
+	printf("%ld\n", (long)Stack_pop(st));
+	Stack_free(&st);
+
 	return 0;
 }
