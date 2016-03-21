adb58ffec7340abea977f7b21d17aa86a4aadfcd
wonghoifung <wonghoifung85@gmail.com>
2016-03-21 19:19:28 +0800

    except

--- cii/except.h
+++ cii/except.h
@@ -10,13 +10,40 @@ typedef struct T {
 } T;
 
 // types
+typedef struct Except_Frame Except_Frame;
+struct Except_Frame {
+	Except_Frame* prev;
+	jmp_buf env;
+	const char* file;
+	int line;
+	const T* exception;
+};
+enum {
+	Except_entered = 0,
+	Except_raised,
+	Except_handled,
+	Except_finalized,
+};
 
 // variables
+extern Except_Frame* Except_stack;
 
 // functions
 void Except_raise(const T* e, const char* file, int line);
 
 // macros
 #define RAISE(e) Except_raise(&(e), __FILE__, __LINE__)
+#define RERAISE(e) Except_raise(Except_frame.exception, Except_frame.file, Except_frame.line)
+
+#define RETURN switch (/*pop*/,0) default: return
+
+#define TRY do { \
+	volatile int Except_flag; \
+	Except_Frame Except_frame; \
+	// push \
+	Except_flag = setjmp(Except_frame.env); \
+	if (Except_flag == Except_entered) {
+
+
 
 #endif
