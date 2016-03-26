c3dfacc67febd34ba2f79af25a23e054137531b6
wonghoifung <wonghoifung85@gmail.com>
2016-03-26 08:53:32 +0800

    table type

--- cii/table.c
+++ cii/table.c
@@ -7,6 +7,18 @@
 #define T Table_T
 
 // types
+struct T {
+	// fields
+	int size;
+	int (*cmp)(const void* x, const void* y);
+	unsigned (*hash)(const void* key);
+	
+	struct binding {
+		struct binding* link;
+		const void* key;
+		void* value;
+	}** buckets;
+};
 
 // static functions
 
