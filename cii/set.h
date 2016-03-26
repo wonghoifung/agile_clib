7a4953687c99d75a60206aae16a87e710696da89
wonghoifung <wonghoifung85@gmail.com>
2016-03-27 07:35:14 +0800

    set basic operations

--- cii/set.h
+++ cii/set.h
@@ -8,6 +8,10 @@ typedef struct T* T;
 // exported functions
 extern T Set_new(int hint, int cmp(const void* x, const void* y), unsigned hash(const void* x));
 extern void Set_free(T* set);
+extern int Set_length(T set);
+extern int Set_member(T set, const void* member);
+extern void Set_put(T set, const void* member);
+extern void* Set_remove(T set, const void* member);
 
 #undef T
 
