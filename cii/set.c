6f04563667f4f906eb6a0e2e3a0d53a0b0b595ab
wonghoifung <wonghoifung85@gmail.com>
2016-03-28 11:01:06 +0800

    Set_inter

--- cii/set.c
+++ cii/set.c
@@ -195,3 +195,39 @@ T Set_union(T s, T t) {
 		return set;
 	}
 }
+
+T Set_inter(T s, T t) {
+	if (s == NULL) {
+		assert(t);
+		return Set_new(t->size, t->cmp, t->hash);
+	} else if (t == NULL) {
+		return Set_new(s->size, s->cmp, s->hash);
+	} else if (s->length < t->length) {
+		return Set_inter(t, s);
+	} else {
+		T set = Set_new(Arith_min(s->size, t->size), s->cmp, s->hash);
+		assert(s->cmp == t->cmp && s->hash == t->hash);
+		{
+			// for each member q in t
+			int i;
+			struct member* q;
+			for (i=0; i<t->size; i++)
+				for (q=t->buckets[i]; q; q=q->link)
+
+					if (Set_member(s, q->member))
+					// add q->member to set
+					{
+						struct member* p;
+						const void* member = q->member;
+						int i = (*set->hash)(member) % set->size;
+						// add member to set
+						NEW(p);
+						p->member = member;
+						p->link = set->buckets[i];
+						set->buckets[i] = p;
+						set->length++;
+					}
+		}
+		return set;
+	}
+}
