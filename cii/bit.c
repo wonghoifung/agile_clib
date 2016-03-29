e9c0c31e117835b16350cd0f8999853e46923ac9
wonghoifung <wonghoifung85@gmail.com>
2016-03-29 18:36:18 +0800

    bit intf

---
+++ cii/bit.c
@@ -0,0 +1,22 @@
+#include <stdarg.h>
+#include <string.h>
+#include "assert.h"
+#include "bit.h"
+#include "mem.h"
+
+#define T Bit_T
+
+struct T {
+	int length;
+	unsigned char* bytes;
+	unsigned long* words;
+};
+
+// macros
+
+// static data
+
+// static functions
+
+// functions
+
