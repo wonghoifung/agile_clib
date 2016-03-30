7856eaa5fc73370d795a9703377cf4855bc58fc2
wonghoifung <wonghoifung85@gmail.com>
2016-03-30 08:03:49 +0800

    bit

--- cii/bit.c
+++ cii/bit.c
@@ -13,10 +13,35 @@ struct T {
 };
 
 // macros
+#define BPW (8 * sizeof (unsigned long))
+#define nwords(len) ((((len) + BPW - 1) & (~(BPW - 1))) / BPW)
 
 // static data
 
 // static functions
 
 // functions
+T Bit_new(int length) {
+	T set;
+	assert(length >= 0);
+	NEW(set);
+	if (length > 0)
+		set->words = CALLOC(nwords(length), sizeof (unsigned long));
+	else 
+		set->words = NULL;
+	set->bytes = (unsigned char*)set->words;
+	set->length = length;
+	return set;
+}
+
+void Bit_free(T* set) {
+	assert(set && *set);
+	FREE((*set)->words);
+	FREE(*set);
+}
+
+int Bit_length(T set) {
+	assert(set);
+	return set->length;
+}
 
