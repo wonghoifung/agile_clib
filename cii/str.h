34a153885b9ab3662622c7b0b48820cd3dedb000
wonghoifung <wonghoifung85@gmail.com>
2016-03-31 08:06:31 +0800

    low level string

--- cii/str.h
+++ cii/str.h
@@ -8,6 +8,12 @@
 //-5 -4 -3 -2 -1 0
 
 // exported functions
+extern char* Str_sub(const char* s, int i, int j);
+extern char* Str_dup(const char* s, int i, int j, int n);
+extern char* Str_cat(const char* s1, int i1, int j1, const char* s2, int i2, int j2);
+extern char* Str_catv(const char* s, ...);
+extern char* Str_reverse(const char* s, int i, int j);
+extern char* Str_map(const char* s, int i, int j, const char* from, const char* to);
 
 #undef T
 
