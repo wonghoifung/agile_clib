f5837d20ffff4262896fb0a47461364c507b3ae5
wonghoifung <wonghoifung85@gmail.com>
2016-04-07 18:41:36 +0800

    thread impl

---
+++ cii/swtch.s
@@ -0,0 +1,21 @@
+#if alpha
+
+; ALPHA swtch
+
+; ALPHA startup
+
+#elif mips
+
+; SPARC swtch
+
+; SPARC startup
+
+#elif sparc
+
+; MIPS swtch
+
+; MIPS startup
+
+#else
+unsupported platform
+#endif
