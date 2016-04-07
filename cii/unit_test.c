8061c076cdb04cb3ae4978c97b8059f9fc3ba039
wonghoifung <wonghoifung85@gmail.com>
2016-04-07 20:20:55 +0800

    test platform

--- cii/unit_test.c
+++ cii/unit_test.c
@@ -3,7 +3,9 @@
 // #include "ring.h"
 // #include "bit.h"
 // #include "fmt.h"
-#include "text.h"
+// #include "text.h"
+
+#include <stdio.h>
 
 int main() {
 	// test_array();
@@ -11,5 +13,21 @@ int main() {
 	// test_ring();
 	// test_bit();
 	// test_fmt();
-	test_text();
+	// test_text();
+
+	#if alpha
+	{
+		printf("alpha\n");
+	}
+	#elif mips
+	{
+		printf("mips\n");
+	}
+	#elif sparc
+	{
+		printf("sparc\n");
+	}
+	#else
+	unsupported platform
+	#endif
 }
