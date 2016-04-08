80494ccd21e23d7f268795422fc5399afbc7247f
wonghoifung <wonghoifung85@gmail.com>
2016-04-07 23:17:14 -0400

    the lib seq

---
+++ cii/build_spin.sh
@@ -0,0 +1,11 @@
+as -o swtch.o swtch.s 
+cc -c -o thread.o thread.c
+cc -c -o mem.o mem.c
+cc -c -o fmt.o fmt.c
+cc -c -o assert.o assert.c
+cc -c -o except.o except.c
+cc -c -o chan.o chan.c
+ar ruv libcii.a mem.o fmt.o assert.o except.o thread.o swtch.o chan.o
+cc -g -c -o spin.o spin.c
+cc -g -o spin spin.o libcii.a
+
