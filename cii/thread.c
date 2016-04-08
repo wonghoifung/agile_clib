4d7ae7557aefecd1905ac7d83f562690841e875d
wonghoifung <wonghoifung85@gmail.com>
2016-04-08 08:22:16 +0800

    sem impl

--- cii/thread.c
+++ cii/thread.c
@@ -319,5 +319,43 @@ T Thread_new(int apply(void*), void* args, int nbytes, ...) {
 #define T Sem_T
 
 // sem functions
+T* Sem_new(int count) {
+	T* s;
+	NEW(s);
+	Sem_init(s, count);
+	return s;
+}
+
+void Sem_init(T* s, int count) {
+	assert(current);
+	assert(s);
+	s->count = count;
+	s->queue = NULL;
+}
+
+void Sem_wait(T* s) {
+	assert(current);
+	assert(s);
+	testalert();
+	if (s->count <= 0) {
+		put(current, (Thread_T*)&s->queue);
+		run();
+		testalert();
+	} else {
+		--s->count;
+	}
+}
+
+void Sem_signal(T* s) {
+	assert(current);
+	assert(s);
+	if (s->count == 0 && !isempty(s->queue)) {
+		Thread_T t = get((Thread_T*)&s->queue);
+		assert(!t->alerted);
+		put(t, &ready);
+	} else {
+		++s->count;
+	}
+}
 
 #undef T
