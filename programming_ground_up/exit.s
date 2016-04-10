4b3fc203bab6a5347e156d0f4f0416c6b8d0b199
wonghoifung <wonghoifung85@gmail.com>
2016-04-10 09:11:16 +0800

    exit.s

--- programming_ground_up/exit.s
+++ programming_ground_up/exit.s
@@ -4,5 +4,5 @@
 .global _start
 _start:
 	movl $1, %eax # linux kernel command number(syscall) for exiting a program
-	movl $0, %ebx # status return to os, check by echo $?
+	movl $888, %ebx # status return to os, check by echo $?
 	int $0x80 # wakes up the kernel to run the exit command
